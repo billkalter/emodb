aec4dd895e28cd25938758fede1e3813f31ff677
billkalter <bill.kalter@bazaarvoice.com>
2016-12-02 22:48:57 -0600

    Removed legacy cache listener

--- databus/src/main/java/com/bazaarvoice/emodb/databus/db/generic/CachingSubscriptionDAO.java
+++ databus/src/main/java/com/bazaarvoice/emodb/databus/db/generic/CachingSubscriptionDAO.java
@@ -17,7 +17,6 @@ import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import com.google.common.cache.RemovalCause;
 import com.google.common.cache.RemovalListener;
-import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import com.google.common.util.concurrent.ListenableFuture;
@@ -28,7 +27,6 @@ import org.joda.time.Duration;
 import java.time.Clock;
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -46,25 +44,12 @@ public class CachingSubscriptionDAO implements SubscriptionDAO {
     private static final OwnedSubscription NULL_SUBSCRIPTION =
             new DefaultOwnedSubscription("__null", Conditions.alwaysFalse(), new Date(0), Duration.ZERO, "__null");
 
-
     private final SubscriptionDAO _delegate;
     private final LoadingCache<String, OwnedSubscription> _subscriptionCache;
     private final LoadingCache<String, List<OwnedSubscription>> _allSubscriptionsCache;
     private final ListeningExecutorService _refreshService;
     private final CacheHandle _subscriptionCacheHandle;
     private final Meter _invalidationEventMeter;
-
-    /**
-     * In a prior version of Emo all subscriptions were maintained in a single cache entry.
-     * In order to support an in-flight upgrade to the current system where each subscription is cached independently
-     * the following cache and cache handle are necessary.  Otherwise, cache invalidations from the legacy system
-     * would not be handled on the current system, nor vice versa.
-     *
-     * Once the entire cluster has been upgraded and there are no more legacy instances active the following
-     * legacy attributes and all code referencing them should be removed.
-     */
-    private final LoadingCache<String, Map<String, OwnedSubscription>> _legacyCache;
-    private final CacheHandle _legacyCacheHandle;
 
     @Inject
     public CachingSubscriptionDAO(@CachingSubscriptionDAODelegate SubscriptionDAO delegate,
@@ -117,10 +102,6 @@ public class CachingSubscriptionDAO implements SubscriptionDAO {
                             // Can't cache null, use special null value
                             ownedSubscription = NULL_SUBSCRIPTION;
                         }
-
-                        // Ensure there is an entry in the legacy cache so it can receive invalidation events.
-                        _legacyCache.get(SUBSCRIPTIONS);
-
                         return ownedSubscription;
                     }
 
@@ -139,25 +120,6 @@ public class CachingSubscriptionDAO implements SubscriptionDAO {
 
         _invalidationEventMeter = metricRegistry.meter(
                 MetricRegistry.name("bv.emodb.databus", "CachingSubscriptionDAO", "invalidation-events"));
-
-        // Register the legacy cache in order to exchange cache invalidations with the legacy system.
-        _legacyCache = CacheBuilder.newBuilder()
-                .removalListener(notification -> {
-                    // The legacy system sends a single notification when any subscription changes.  Without knowing
-                    // which subscription changed the only safe action is to invalidate them all.  This is inefficient
-                    // but is necessary only for the brief period after the new stack is deployed and the legacy
-                    // stack hasn't yet been torn down.
-                    _subscriptionCache.invalidateAll();
-                })
-                .build(new CacheLoader<String, Map<String, OwnedSubscription>>() {
-                    @Override
-                    public Map<String, OwnedSubscription> load(String key) throws Exception {
-                        // The actual cached object doesn't matter since this cache is only used for receiving
-                        // invalidation messages.  Just need to provide a non-null value.
-                        return ImmutableMap.of();
-                    }
-                });
-        _legacyCacheHandle = cacheRegistry.register("subscriptions", _legacyCache, true);
     }
 
     @Override
@@ -181,10 +143,6 @@ public class CachingSubscriptionDAO implements SubscriptionDAO {
     private void invalidateSubscription(String subscription) {
         _invalidationEventMeter.mark();
         _subscriptionCacheHandle.invalidate(InvalidationScope.DATA_CENTER, subscription);
-
-        // Send notification to the legacy stack that all subscriptions must be invalidated since it only supports
-        // full cache invalidation.
-        _legacyCacheHandle.invalidate(InvalidationScope.DATA_CENTER, SUBSCRIPTIONS);
     }
 
     @Override
